97021467b7d8eeef98f6d3f498d94a1c39e4caa8
Ömer Avcı <111347166+oomeravcii@users.noreply.github.com>
2023-04-27 16:50:30 +0300

    Update file_management_english.py

--- file_management_english.py
+++ file_management_english.py
@@ -9,7 +9,7 @@
 
 #! File reading "r" -> The file must exist.
 
-# file = open("planlar.txt","r",encoding="utf-8") #? Opens the file. We use "utf-8" to write Turkish characters.
+# file = open("plans.txt","r",encoding="utf-8") #? Opens the file. We use "utf-8" to write Turkish characters.
 
 # print(file.read()) #? Reads the file and prints it to the screen.
 # print(file.read(50)) #? Reads the specified number of characters from the file and prints them to the screen.
@@ -25,7 +25,7 @@
 
 #! File writing "w" -> If the file does not exist, it is automatically created. (Overwrites existing content.)
 
-# file = open("planlar.txt","w",encoding="utf-8")
+# file = open("plans.txt","w",encoding="utf-8")
 
 # file.write("Hello") #? Writes text to the file.
 # file.writelines(["7:30 Breakfast\n","9:00 Running\n","11:00 Reading a Book\n"]) #? Writes lines as a list of items.
@@ -36,7 +36,7 @@
 
 #! File writing "a" -> If the file does not exist, it is automatically created. (Starts writing at the end of existing content.)
 
-# file = open("planlar.txt","a",encoding="utf-8")
+# file = open("plans.txt","a",encoding="utf-8")
 
 # file.write("12:00 Sightseeing\n") #? Writes text to the file.
 
@@ -46,6 +46,6 @@
 
 # Files can also be used this way. (Files are automatically closed after the operation is finished.)
 
-# with open("planlar.txt","w",encoding="utf-8") as file:
+# with open("plans.txt","w",encoding="utf-8") as file:
 #    file.write("14:00 Reading a Book\n")
 #    file.writelines(["15:00 Swimming Lesson\n","16:00 Running\n"])
